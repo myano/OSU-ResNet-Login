a7730e41d4c2018b5ed17beef5ba8a8292c1afcd
Michael S. Yanovich <yanovich.1@osu.edu>
2011-05-14 03:13:22 -0400

    Changed the 404 link and make it PEP8 compliant

--- resnet.py
+++ resnet.py
@@ -22,6 +22,7 @@ import urllib
 import re
 import getpass
 
+
 def get_login_url():
     """Returns login url.
 
@@ -32,9 +33,7 @@ def get_login_url():
 
 
     #See if we're logged in already
-    #If we are, this will go through to xkcd, which returns a 404
-    #If we aren't this will let us find the login url
-    conn = urllib.urlopen("http://www.xkcd.com/404")
+    conn = urllib.urlopen("http://www.google.com/404")
     code = conn.getcode()
     data = conn.read()
     conn.close()
@@ -50,7 +49,7 @@ def get_login_url():
             #No result found - time to die
             #This probably means OSU changes the ResNet response page
             raise Exception("Could not find login url")
-        
+
         #Return the URL
         return url.group(0)
 
@@ -58,13 +57,16 @@ def get_login_url():
         #A good response means we're already signed in
         return None
 
+
 def get_captive_form(url):
     """Downloads the login form from ResNet.  Returns (loginurl, params).
 
-    The url passed to this function should be the return value of get_login_url.
+    The url passed to this function should be the return value of get_login_url
 
     The loginurl returned is the url to submit the login response to.
-    The params should be POST'ed with the login response.  They are a collection of random name/value pairs passed in hidden inputs.  Better just to pass them along; I think they're important.
+    The params should be POST'ed with the login response.  They are a
+    collection of random name/value pairs passed in hidden inputs.  Better just
+    to pass them along; I think they're important.
     """
 
     #Connect to the login form
@@ -77,7 +79,8 @@ def get_captive_form(url):
         raise Exception("Failed to retrieve login form")
 
     #Find the form's path
-    loginurl =  re.search('<form method="post" name="loginform" action="([^"]+)"', data)
+    loginurl = re.search('<form method="post" name="loginform" \
+            action="([^"]+)"', data)
     if loginurl is None:
         raise Exception("Could not find form path")
     loginurl = loginurl.group(1)
@@ -89,13 +92,15 @@ def get_captive_form(url):
 
     #The re searches for hidden inputs.
     #Be careful, it's not pretty.
-    hidden_inputs = re.findall(r"""<input\s+type\s*=\s*(["'])hidden\1\s+name\s*=\s*(["'])([^"']+)\2\s+value\s*=\s*(["'])([^"']+)\4\s*/>""", data)
+    hidden_inputs = re.findall(r"""<input\s+type\s*=\s*(["'])hidden\1\s+name\
+            s*=\s*(["'])([^"']+)\2\s+value\s*=\s*(["'])([^"']+)\4\s*/>""",
+            data)
 
     #Now iterate over the hidden inputs, and add them to the params list
     #a, b, and d are random quotes that we don't care about now
     #but that we needed to properly match the re
     params = {}
-    for a,b, name, d, value in hidden_inputs:
+    for a, b, name, d value in hidden_inputs:
         params[name] = value
 
     #MODIFY THIS PART
@@ -103,16 +108,17 @@ def get_captive_form(url):
     #Put in your name.number inside the quotes
     params["username"] = "name.1"
 
-
     params["password"] = getpass.getpass("OSU Login Password: ")
 
     #If you want, you can comment the above line and uncomment the code below
-    #Then put your password in plaintext in the quotes, and you won't have to enter it
-    #Of course, then anyone could just read your password if they look in this file
+    #Then put your password in plaintext in the quotes, and you won't have to
+    #enter it Of course, then anyone could just read your password if they
+    #look in this file
 
     #params["password"] = "your password in plaintext here"
 
     return (loginurl, params)
+
 
 def do_login(loginurl, params):
     conn = urllib.urlopen(loginurl, urllib.urlencode(params))
@@ -124,7 +130,6 @@ def do_login(loginurl, params):
         raise Warning("Could not confirm success")
 
 
-
 if __name__ == "__main__":
     url = get_login_url()
     if url is None:
@@ -132,4 +137,3 @@ if __name__ == "__main__":
     else:
         (loginurl, params) = get_captive_form(url)
         do_login(loginurl, params)
-
